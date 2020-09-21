5d90f10f09be9a068135c3e372e74d409e533d9b
Guilherme Capanema <guilherme@capanema.me>
2020-09-21 11:02:10 -0300

    Adiciona modelos para veículo e rota

---
+++ app/Models/Route.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Route extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    const TYPE_BUS = 3;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+}
---
+++ database/migrations/2020_09_21_120922_create_routes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoutesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id();
+            $table->string('json_id')
+                ->unique();
+            $table->string('short_name');
+            $table->string('long_name');
+            $table->integer('type');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routes');
+    }
+}
