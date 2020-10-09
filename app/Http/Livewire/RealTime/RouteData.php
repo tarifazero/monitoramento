17fff6f3c309565ef34c7eb33c881d5c0c9b1dba
Guilherme Capanema <guilherme@capanema.me>
2020-10-09 09:28:25 -0300

    Exibe percentuais da frota nos dados globais

--- app/Http/Livewire/RealTime/RouteData.php
+++ app/Http/Livewire/RealTime/RouteData.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire\RealTime;
 use App\Models\RealTimeEntry;
 use App\Models\Route;
 use App\Models\RouteVehicle;
+use App\Models\Vehicle;
+use Carbon\Carbon;
 use DateTimeZone;
 use Livewire\Component;
 
@@ -26,27 +28,56 @@ class RouteData extends Component
             ->count();
     }
 
-    public function getVehicleCountByHourProperty()
+    public function getTotalVehicleCountProperty()
     {
-        $countByHour = collect();
+        return Vehicle::count();
+    }
+
+    public function getRouteVehiclesByHourProperty()
+    {
+        $startOfDay = today(new DateTimeZone(config('app.display_timezone')))
+            ->startOfDay()
+            ->setTimezone(config('app.timezone'));
+
+        $endOfDay = today(new DateTimeZone(config('app.display_timezone')))
+            ->endOfDay()
+            ->setTimezone(config('app.timezone'));
+
+        return RouteVehicle::where('created_at', '>=', $startOfDay)
+            ->where('created_at', '<=', $endOfDay)
+            ->when($this->route, function ($query, $route) {
+                $query->whereRouteWithChildren($this->route);
+            })
+            ->get()
+            ->groupBy('created_at')
+            ->mapWithKeys(function ($value, $key) {
+                $hour = Carbon::parse($key)
+                    ->setTimezone(config('app.display_timezone'))
+                    ->hour;
+
+                return [
+                    $hour => $value,
+                ];
+            });
+    }
+
+    public function getStatsByHourProperty()
+    {
+        $statsByHour = collect();
 
         foreach (range(0, 23) as $hour) {
-            $countByHour->put(
+            $vehicleCount = count($this->routeVehiclesByHour->get($hour, []));
+
+            $statsByHour->put(
                 $hour,
-                RouteVehicle::where(
-                    'created_at',
-                    today(new DateTimeZone(config('app.display_timezone')))
-                        ->hour($hour)
-                        ->setTimezone(config('app.timezone'))
-                )
-                    ->when($this->route, function ($query, $route) {
-                        $query->whereRouteWithChildren($this->route);
-                    })
-                    ->count()
+                [
+                    'vehicle_count' => $vehicleCount,
+                    'vehicle_percentage' => 100 * $vehicleCount / $this->totalVehicleCount,
+                ]
             );
         }
 
-        return $countByHour;
+        return $statsByHour;
     }
 
     public function routeSelected(Route $route)
--- resources/views/livewire/real-time/route-data.blade.php
+++ resources/views/livewire/real-time/route-data.blade.php
@@ -23,24 +23,36 @@
                         Hora
                     </th>
 
-                    <th class="text-center px-4 py-1">
-                        Número de veículos
-                    </th>
+                    @if ($route)
+                        <th class="text-center px-4 py-1">
+                            Número de veículos
+                        </th>
+                    @else
+                        <th class="text-center px-4 py-1">
+                            Percentual da frota
+                        </th>
+                    @endif
 
                 </tr>
             </thead>
 
             <tbody>
-                @foreach ($this->vehicleCountByHour as $hour => $count)
+                @foreach ($this->statsByHour as $hour => $stats)
                     <tr>
 
                         <td class="text-center px-4 py-1">
                             {{ $hour }}
                         </td>
 
-                        <td class="text-center px-4 py-1">
-                            {{ $count }}
-                        </td>
+                        @if ($route)
+                            <td class="text-center px-4 py-1">
+                                {{ $stats['vehicle_count'] }}
+                            </td>
+                        @else
+                            <td class="text-center px-4 py-1">
+                                {{ $stats['vehicle_percentage'] }}%
+                            </td>
+                        @endif
 
                     </tr>
                 @endforeach
