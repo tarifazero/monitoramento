6c24a8d42a8a76c35ce92d7443337bb37cecdaab
Guilherme Capanema <guilherme@capanema.me>
2020-11-10 15:06:39 -0300

    Remove import desnecessário

--- tests/Feature/Commands/Gtfs/ProcessStopsTest.php
+++ tests/Feature/Commands/Gtfs/ProcessStopsTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature\Commands\Gtfs;
 
 use App\Models\GtfsFetch;
 use App\Models\Stop;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Storage;
