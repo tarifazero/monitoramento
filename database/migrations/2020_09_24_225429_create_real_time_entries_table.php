790652d0f986700bca021ac3445652da047ecaad
Guilherme Capanema de Barros <guilherme@capanema.me>
2021-02-12 20:21:56 -0300

    Ajusta esquema das real_time_entries e define como hypertable

--- database/migrations/2020_09_24_225429_create_real_time_entries_table.php
+++ database/migrations/2020_09_24_225429_create_real_time_entries_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateRealTimeEntriesTable extends Migration
@@ -9,22 +10,20 @@ class CreateRealTimeEntriesTable extends Migration
     /**
      * Run the migrations.
      *
-     * @return void */ public function up()
+     * @return void
+     */
+    public function up()
     {
         Schema::create('real_time_entries', function (Blueprint $table) {
-            $table->uuid('id')
-                  ->primary();
+            $table->dateTime('timestamp');
             $table->integer('vehicle_id')
                   ->constrained()
                   ->onDelete('cascade');
             $table->integer('route_id')
                   ->constrained()
                   ->onDelete('cascade');
-            $table->dateTime('timestamp')
-                  ->index();
             $table->double('latitude');
             $table->double('longitude');
-            $table->integer('speed');
             $table->integer('travel_direction')
                   ->nullable();
         });
