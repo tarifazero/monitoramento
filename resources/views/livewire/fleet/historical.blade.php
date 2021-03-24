c539ae2430a945cd7e4d3d2372e5323d41823174
Guilherme Capanema de Barros <guilherme@capanema.me>
2021-03-24 12:19:30 -0300

    Move seção de média da frota do dia anterior para componente correto

--- resources/views/livewire/fleet/historical.blade.php
+++ resources/views/livewire/fleet/historical.blade.php
@@ -1,107 +1,78 @@
-<div>
-    <div class="flex text-xl font-dm font-bold bg-beige-light flex-grow rounded-2xl">
-
-        <div class="relative text-center bg-yellow-dark p-4 lg:p-6 w-3/5 rounded-2xl">
-            <p class="flex flex-col items-center justify-center h-full">
-
-                <span>
-                    Ontem, a média diária da frota circulante foi de
-                </span>
-
-                <span class="block text-4xl font-work font-extrabold">
-                    {{ $this->dailyAverageActiveFleet->last()['value'] }}%
-                </span>
-
-            </p>
-
-            <div class="absolute right-0 top-1/2 bg-yellow-dark w-4 h-4 transform rotate-45 -translate-y-1/2 translate-x-1/2"></div>
-        </div>
-
-        <div class="relative text-center p-4 lg:p-6 w-2/5">
-            <p class="flex flex-col items-center justify-center h-full">
-                Compare este valor com as médias dos últimos dias!
-            </p>
-        </div>
-
-    </div>
-
-    <div class="bg-beige-light p-6 rounded-2xl mt-12">
-        <canvas
-            x-data="{ chart: null}"
-            x-init="() => {
-                this.chart = new Chart($el, {
-                    type: 'line',
-                    data: {
-                        labels: JSON.parse('{{ json_encode($this->dailyAverageActiveFleet->pluck('label')) }}'),
-                        datasets: [{
-                            data: JSON.parse('{{ json_encode($this->dailyAverageActiveFleet->pluck('value')) }}'),
-                            borderColor: '#E6007D',
-                            borderWidth: 6,
-                            backgroundColor: 'transparent',
+<div class="bg-beige-light p-6 rounded-2xl">
+    <canvas
+        x-data="{ chart: null}"
+        x-init="() => {
+            this.chart = new Chart($el, {
+                type: 'line',
+                data: {
+                    labels: JSON.parse('{{ json_encode($this->dailyAverageActiveFleet->pluck('label')) }}'),
+                    datasets: [{
+                        data: JSON.parse('{{ json_encode($this->dailyAverageActiveFleet->pluck('value')) }}'),
+                        borderColor: '#E6007D',
+                        borderWidth: 6,
+                        backgroundColor: 'transparent',
+                    }],
+                },
+                options: {
+                    maintainAspectRatio: false,
+                    scales: {
+                        xAxes: [{
+                            gridLines: {
+                                color: '#E6007D',
+                                zeroLineColor: '#E6007D',
+                                drawBorder: false,
+                            },
+                            ticks: {
+                                fontStyle: 700,
+                                fontColor: '#000000',
+                                fontSize: 16,
+                                autoSkip: true,
+                                maxTicksLimit: 6,
+                            },
+                        }],
+                        yAxes: [{
+                            gridLines: {
+                                color: '#E6007D',
+                                zeroLineColor: '#E6007D',
+                                drawBorder: false,
+                            },
+                            ticks: {
+                                beginAtZero: true,
+                                max: 100,
+                                stepSize: 25,
+                                fontStyle: 700,
+                                fontColor: '#000000',
+                                fontSize: 20,
+                                padding: 30,
+                                callback: (tick) => tick + '%',
+                            },
                         }],
                     },
-                    options: {
-                        maintainAspectRatio: false,
-                        scales: {
-                            xAxes: [{
-                                gridLines: {
-                                    color: '#E6007D',
-                                    zeroLineColor: '#E6007D',
-                                    drawBorder: false,
-                                },
-                                ticks: {
-                                    fontStyle: 700,
-                                    fontColor: '#000000',
-                                    fontSize: 16,
-                                    autoSkip: true,
-                                    maxTicksLimit: 6,
-                                },
-                            }],
-                            yAxes: [{
-                                gridLines: {
-                                    color: '#E6007D',
-                                    zeroLineColor: '#E6007D',
-                                    drawBorder: false,
-                                },
-                                ticks: {
-                                    beginAtZero: true,
-                                    max: 100,
-                                    stepSize: 25,
-                                    fontStyle: 700,
-                                    fontColor: '#000000',
-                                    fontSize: 20,
-                                    padding: 30,
-                                    callback: (tick) => tick + '%',
-                                },
-                            }],
-                        },
-                        legend: {
-                            display: false,
-                        },
-                        tooltips: {
-                            backgroundColor: '#EC008C',
-                            titleFontSize: 20,
-                            titleFontFamily: 'Work Sans',
-                            titleFontStyle: 700,
-                            titleAlign: 'center',
-                            titleMarginBottom: 10,
-                            bodyFontSize: 28,
-                            bodyFontFamily: 'Work Sans',
-                            bodyFontStyle: 700,
-                            bodyAlign: 'center',
-                            xPadding: 24,
-                            yPadding: 16,
-                            displayColors: false,
-                            callbacks: {
-                                title: (title) => title[0].label.toUpperCase(),
-                                label: (label) => label.yLabel + '%',
-                            },
+                    legend: {
+                        display: false,
+                    },
+                    tooltips: {
+                        backgroundColor: '#EC008C',
+                        titleFontSize: 20,
+                        titleFontFamily: 'Work Sans',
+                        titleFontStyle: 700,
+                        titleAlign: 'center',
+                        titleMarginBottom: 10,
+                        bodyFontSize: 28,
+                        bodyFontFamily: 'Work Sans',
+                        bodyFontStyle: 700,
+                        bodyAlign: 'center',
+                        xPadding: 24,
+                        yPadding: 16,
+                        displayColors: false,
+                        callbacks: {
+                            title: (title) => title[0].label.toUpperCase(),
+                            label: (label) => label.yLabel + '%',
                         },
                     },
-                });
-            }"
-            height="500"
-        ></canvas>
-    </div>
-
+                },
+            });
+        }"
+        height="500"
+    ></canvas>
 </div>
--- resources/views/livewire/fleet/yesterday.blade.php
+++ resources/views/livewire/fleet/yesterday.blade.php
@@ -1,80 +1,111 @@
-<div class="bg-beige-light p-6 rounded-2xl">
-    <h2 class="text-4xl text-center font-work font-extrabold">
-        {{ $this->yesterday->format('d/m/Y') }}
-    </h2>
+<div>
+    <div class="bg-beige-light p-6 rounded-2xl">
 
-    <div class="mt-6">
-        <canvas
-            x-data="{ chart: null}"
-            x-init="() => {
-                this.chart = new Chart($el, {
-                    type: 'line',
-                    data: {
-                        labels: JSON.parse('{{ json_encode($this->hourlyActiveFleet->pluck('label')) }}'),
-                        datasets: [{
-                            data: JSON.parse('{{ json_encode($this->hourlyActiveFleet->pluck('value')) }}'),
-                            borderColor: '#FFCA44',
-                            borderWidth: 6,
-                            backgroundColor: 'rgba(255, 235, 107, 0.5)',
-                        }],
-                    },
-                    options: {
-                        maintainAspectRatio: false,
-                        scales: {
-                            xAxes: [{
-                                gridLines: {
-                                    display:false,
-                                },
-                                ticks: {
-                                    fontStyle: 700,
-                                    fontColor: '#000000',
-                                    fontSize: 16,
-                                },
-                            }],
-                            yAxes: [{
-                                gridLines: {
-                                    color: '#E6007D',
-                                    zeroLineColor: '#E6007D',
-                                    drawBorder: false,
-                                },
-                                ticks: {
-                                    beginAtZero: true,
-                                    max: 100,
-                                    stepSize: 25,
-                                    fontStyle: 700,
-                                    fontColor: '#000000',
-                                    fontSize: 20,
-                                    padding: 30,
-                                    callback: (tick) => tick + '%',
-                                },
+        <h2 class="text-4xl text-center font-work font-extrabold">
+            {{ $this->yesterday->format('d/m/Y') }}
+        </h2>
+
+        <div class="mt-6">
+            <canvas
+                x-data="{ chart: null}"
+                x-init="() => {
+                    this.chart = new Chart($el, {
+                        type: 'line',
+                        data: {
+                            labels: JSON.parse('{{ json_encode($this->hourlyActiveFleet->pluck('label')) }}'),
+                            datasets: [{
+                                data: JSON.parse('{{ json_encode($this->hourlyActiveFleet->pluck('value')) }}'),
+                                borderColor: '#FFCA44',
+                                borderWidth: 6,
+                                backgroundColor: 'rgba(255, 235, 107, 0.5)',
                             }],
                         },
-                        legend: {
-                            display: false,
-                        },
-                        tooltips: {
-                            backgroundColor: '#EC008C',
-                            titleFontSize: 20,
-                            titleFontFamily: 'Work Sans',
-                            titleFontStyle: 700,
-                            titleAlign: 'center',
-                            titleMarginBottom: 10,
-                            bodyFontSize: 28,
-                            bodyFontFamily: 'Work Sans',
-                            bodyFontStyle: 700,
-                            bodyAlign: 'center',
-                            xPadding: 24,
-                            yPadding: 16,
-                            displayColors: false,
-                            callbacks: {
-                                title: (title) => title[0].label.toUpperCase(),
-                                label: (label) => label.yLabel + '%',
+                        options: {
+                            maintainAspectRatio: false,
+                            scales: {
+                                xAxes: [{
+                                    gridLines: {
+                                        display:false,
+                                    },
+                                    ticks: {
+                                        fontStyle: 700,
+                                        fontColor: '#000000',
+                                        fontSize: 16,
+                                    },
+                                }],
+                                yAxes: [{
+                                    gridLines: {
+                                        color: '#E6007D',
+                                        zeroLineColor: '#E6007D',
+                                        drawBorder: false,
+                                    },
+                                    ticks: {
+                                        beginAtZero: true,
+                                        max: 100,
+                                        stepSize: 25,
+                                        fontStyle: 700,
+                                        fontColor: '#000000',
+                                        fontSize: 20,
+                                        padding: 30,
+                                        callback: (tick) => tick + '%',
+                                    },
+                                }],
+                            },
+                            legend: {
+                                display: false,
+                            },
+                            tooltips: {
+                                backgroundColor: '#EC008C',
+                                titleFontSize: 20,
+                                titleFontFamily: 'Work Sans',
+                                titleFontStyle: 700,
+                                titleAlign: 'center',
+                                titleMarginBottom: 10,
+                                bodyFontSize: 28,
+                                bodyFontFamily: 'Work Sans',
+                                bodyFontStyle: 700,
+                                bodyAlign: 'center',
+                                xPadding: 24,
+                                yPadding: 16,
+                                displayColors: false,
+                                callbacks: {
+                                    title: (title) => title[0].label.toUpperCase(),
+                                    label: (label) => label.yLabel + '%',
+                                },
                             },
                         },
-                    },
-                });
-            }"
-            height="500"
-        ></canvas>
+                    });
+                }"
+                height="500"
+            ></canvas>
+        </div>
+
     </div>
+
+    <div class="flex text-xl font-dm font-bold bg-beige-light flex-grow rounded-2xl mt-12">
+
+        <div class="relative text-center bg-yellow-dark p-4 lg:p-6 w-3/5 rounded-2xl">
+            <p class="flex flex-col items-center justify-center h-full">
+
+                <span>
+                    Ontem, a média diária da frota circulante foi de
+                </span>
+
+                <span class="block text-4xl font-work font-extrabold">
+                    {{ $this->hourlyActiveFleet->average('value') }}%
+                </span>
+
+            </p>
+
+            <div class="absolute right-0 top-1/2 bg-yellow-dark w-4 h-4 transform rotate-45 -translate-y-1/2 translate-x-1/2"></div>
+        </div>
+
+        <div class="relative text-center p-4 lg:p-6 w-2/5">
+            <p class="flex flex-col items-center justify-center h-full">
+                Compare este valor com as médias dos últimos dias!
+            </p>
+        </div>
+
+    </div>
+
 </div>
