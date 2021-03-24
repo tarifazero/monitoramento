59fbc0e3053fd777abf502aa24ab5f46f06c1e5d
Guilherme Capanema de Barros <guilherme@capanema.me>
2021-03-24 11:23:55 -0300

    Inline o componente de gráfico, que não seria reaproveitado

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
 require('./bootstrap');
 
 window.Chart = require('chart.js');
+Chart.defaults.global.defaultFontFamily = 'DM Sans';
--- resources/views/livewire/fleet/yesterday.blade.php
+++ resources/views/livewire/fleet/yesterday.blade.php
@@ -4,9 +4,77 @@
     </h2>
 
     <div class="mt-6">
-        <x-chart
-            :labels="$this->hourlyActiveFleet->pluck('label')"
-            :values="$this->hourlyActiveFleet->pluck('value')"
-        />
+        <canvas
+            x-data="{ chart: null}"
+            x-init="() => {
+                this.chart = new Chart($el, {
+                    type: 'line',
+                    data: {
+                        labels: JSON.parse('{{ json_encode($this->hourlyActiveFleet->pluck('label')) }}'),
+                        datasets: [{
+                            data: JSON.parse('{{ json_encode($this->hourlyActiveFleet->pluck('value')) }}'),
+                            borderColor: '#FFCA44',
+                            borderWidth: 6,
+                            backgroundColor: 'rgba(255, 235, 107, 0.5)',
+                        }],
+                    },
+                    options: {
+                        maintainAspectRatio: false,
+                        scales: {
+                            xAxes: [{
+                                gridLines: {
+                                    display:false,
+                                },
+                                ticks: {
+                                    fontStyle: 700,
+                                    fontColor: '#000000',
+                                    fontSize: 16,
+                                },
+                            }],
+                            yAxes: [{
+                                gridLines: {
+                                    color: '#E6007D',
+                                    zeroLineColor: '#E6007D',
+                                    drawBorder: false,
+                                },
+                                ticks: {
+                                    beginAtZero: true,
+                                    max: 100,
+                                    stepSize: 25,
+                                    fontStyle: 700,
+                                    fontColor: '#000000',
+                                    fontSize: 20,
+                                    padding: 30,
+                                    callback: (tick) => tick + '%',
+                                },
+                            }],
+                        },
+                        legend: {
+                            display: false,
+                        },
+                        tooltips: {
+                            backgroundColor: '#EC008C',
+                            titleFontSize: 20,
+                            titleFontFamily: 'Work Sans',
+                            titleFontStyle: 700,
+                            titleAlign: 'center',
+                            titleMarginBottom: 10,
+                            bodyFontSize: 28,
+                            bodyFontFamily: 'Work Sans',
+                            bodyFontStyle: 700,
+                            bodyAlign: 'center',
+                            xPadding: 24,
+                            yPadding: 16,
+                            displayColors: false,
+                            callbacks: {
+                                title: (title) => title[0].label.toUpperCase(),
+                                label: (label) => label.yLabel + '%',
+                            },
+                        },
+                    },
+                });
+            }"
+            height="500"
+        ></canvas>
     </div>
 </div>
