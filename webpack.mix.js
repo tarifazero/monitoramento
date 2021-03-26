aba5cca562046ab67d3fb0a696a6040003a3cb79
Guilherme Capanema de Barros <guilherme@capanema.me>
2021-03-26 15:48:43 -0300

    Adiciona cache-busting dos recursos

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,4 +20,5 @@ mix.js('resources/js/app.js', 'public/js')
     })
     .postCss('resources/css/app.css', 'public/css', [
         tailwindcss('tailwind.config.js'),
-    ]);
+    ])
+    .version();
