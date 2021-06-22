59194131dad48dcf86764c93453c7d4ae06ef5ae
Guilherme Capanema de Barros <guilherme@capanema.me>
2021-06-21 21:06:58 -0300

    Adiciona mensageiro JS para redimensionar iframe

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,11 @@ window.Chart = require('chart.js');
 Chart.defaults.global.defaultFontFamily = 'DM Sans';
 
 window.enquire = require('enquire.js');
+
+document.addEventListener('DOMContentLoaded', function () {
+    setInterval(function() {
+        var height = document.body.scrollHeight;
+        parent.postMessage('resize::' + height, '*');
+    }, 100);
+});
+
