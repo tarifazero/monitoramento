427b7858935e1f468c8000ea13a9481b2467b529
Guilherme Capanema de Barros <guilherme@capanema.me>
2021-07-04 17:57:01 -0300

    Adiciona política de compressão do TimescaleDB para registros de tempo real

---
+++ database/migrations/2021_07_04_194455_create_real_time_entries_compression_policy.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateRealTimeEntriesCompressionPolicy extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("ALTER TABLE real_time_entries SET (timescaledb.compress, timescaledb.compress_orderby = 'timestamp', timescaledb.compress_segmentby = 'vehicle_id, route_id');");
+        DB::statement("SELECT add_compression_policy('real_time_entries', INTERVAL '3 months', true);");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement("SELECT remove_compression_policy('real_time_entries');");
+        DB::statement("SELECT decompress_chunk(i, true) from show_chunks('real_time_entries') i;");
+    }
+}
